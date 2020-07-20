566804477d8dda23b2f2c5162022d0294615613c
dgihring <dgihring@ucsc.edu>
2020-07-20 10:39:02 -0700

    Now the characters can equip and un equip a bag

--- Character.py
+++ Character.py
@@ -11,26 +11,26 @@ class Character:
 
     """Character class, contains information on the character."""
        
-    def __init__(self, name, race, symbol, inventory, health, level, exp):
+    def __init__(self, name, race, symbol, health, level, exp):
         """
         Constructor for Item Class.
         
         Arguments:
             name (string): name of the Character
             race (string): Race of the Character
-            symbol (string): Text Symbol for the character
-            inventory (Inventory): storage 
+            symbol (string): Text Symbol for the character 
             health (int): Health value
             level (int): level of the character
             exp (int): experince points
         """
         
-        
+        nullBag = inv.Inventory("null", 0)
         
         self.name = name
         self.race = race
         self.symbol = symbol
-        self.inventory = inv.Inventory("null", 0)
+        
+        self.bag = nullBag
         self.health = health
         self.level = level
         self.exp = exp
@@ -43,21 +43,29 @@ class Character:
         print("Their name is " + self.name + ".")
         print("They are a " + self.race + ".")
         print("Their Symbol is " + self.symbol + ".")
-        print("Their health is at " + self.health + ".")
-        print("They are level " + self.level+ ".")
-        print("They are this experinced " + self.experince + ".")
+        print("Their health is at " + str(self.health) + ".")
+        print("They are level " + str(self.level) + ".")
+        print("They are this experinced " + str(self.exp) + ".")
         
-    def equipInv(self, inv.Inventory):
-        """ Equips a bag to the inventory slot if empty"""
-        if(self.inventory.name == "null"):
-            self.inventory = inv.Inventory
-            print("Equipped the " + inv.Inventory.name + ".")
-        else:
-            print("Allready have a " + self.inventory.name + " equiped.")  
+    def equipInv(self, inventory):
+         """ Equips a bag to the inventory slot if empty"""
+         if(self.bag.name == "null"):
+             self.bag = inventory
+             print("Equipped the " + inventory.name + ".")
+         else:
+             print("Allready have a " + self.bag.name + " equiped.")  
 
 
 
 def main(): # more for testing than anything
+    Player = Character("James", "Human", "@", 10, 1, 0)
+    Player.info()
+    
+    satchel = inv.Inventory("satchel", 5)
+    Player.equipInv(satchel)
+    purse = inv.Inventory("purse", 4)
+    Player.equipInv(purse)
+    
 
 if __name__ == "__main__":
     main()
