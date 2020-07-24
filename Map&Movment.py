413638b033a3ce46a1659e99a074a70c83ba4cce
dgihring <dgihring@ucsc.edu>
2020-07-24 10:32:33 -0700

    Started creating the player navigator

--- Map&Movment.py
+++ Map&Movment.py
@@ -5,7 +5,9 @@
 Handles creation and the state of the map, as well as the movment of the player.
 """
 
-border = 2
+import Character as Ch
+
+border = 2 # This allows for the walls to be placed around the room's floor
 
 class Tile:
     """Standard movment tile, basiclly acts just as a floor"""
@@ -24,14 +26,23 @@ class Tile:
 class Map:
     """ The Map class, creates itself and manages such"""
     def __init__(self, length, width):
-        """ Initializes the map witha  length and a width"""
+        """ 
+        Initializes the map witha  length and a width
+        
+        Arguments:
+            length (int): defines the x direction of the room
+            width (int): defines the y direction of the room
+            
+        Creates a empty floor space with a given width and length, and then 
+        fills in the outside area with a impassable wall
+        """
         self.length = length
         self.width = width
         self.board = [[Tile("floor",0,".") for x in range(self.width+border)]
                             for y in range (self.length+border)]
         # Sets up walls for the room
-        for j in range(self.width+2):
-            for i in range(self.length+2):
+        for j in range(self.width+border):
+            for i in range(self.length+border):
                 if((i == 0 or i == self.length+1) or
                     (j == 0 or j == self.width+1)):
                     self.board[i][j] = Tile("wall",1,"#")
@@ -42,7 +53,15 @@ class Map:
             for i in range(self.length+border):
                 print(self.board[i][j].symbol, end = " ")
             print("")
-        
+            
+class PlayerNavigator:
+        def __init__(self, Character,map):
+            self.Player = Character
+            self.PosX = 1
+            self.PosY = 1
+            self.symbol = "@"
+            map.board[self.PosX][self.PosY] = self
+            
         
         
         
@@ -59,6 +78,11 @@ def main(): # more for testing than anything
     testMap2 = Map(6,8)
     testMap2.ShowMap()
     testMap3 = Map(10,4)
+    testMap3.ShowMap()
+    print("")
+    print("Testing out adding the navigator to the map")
+    James = Ch.Character("James", "Human", "@", 10, 1, 0)
+    nav = PlayerNavigator(James, testMap3)
     testMap3.ShowMap()
     
 
